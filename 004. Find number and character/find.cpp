983eac6c89df55518cf6ca0c14123e97f7e8e6c7
Rachit <111979590+rachitkatariya@users.noreply.github.com>
2023-09-30 21:37:40 +0530

    Create find.cpp

---
+++ 004. Find number and character/find.cpp
@@ -0,0 +1,23 @@
+// Find number, character and etc
+
+/* 
+#include<iostream>
+using namespace std;
+int main (){
+    char ch;
+    cout << "Enter any Character";
+    cin >> ch;
+     if(ch >= 'A' && ch <= 'Z') {
+        cout << ch << " is Upper case";
+     }
+     else if( ch>= 'a' && ch <= 'z') {
+        cout << ch << " is lower case";
+     }
+     else if( ch>= '0' && ch <= '9' ){
+        cout << ch << " is a number";
+     }
+     else {
+        cout << ch << " is a special character";
+     }
+     return 0;
+} */
