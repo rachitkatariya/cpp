870c365cfe55e6e9fc3ff5939ee777e8240f901d
Rachit <rachitkumar2953@gmail.com>
2023-10-09 20:49:53 +0530

    Create array.cpp

---
+++ 000. Coder Army/array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+// arr[]={1,2,3,4,5};
+// arr[5]={1,2,3};
+// arr[5]={0};
+
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+
+    // input from a user
+
+    // int arr[5];
+    // for(int i=0; i<5; i++)
+    // cin>>arr[i];
+
+    for(int i =0; i<5; i++)
+    cout<<arr[i]<<" ";
+
+    return 0;
+}
+
